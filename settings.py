63f1bc9d4a4a17c1a3dd034ccf0b8139015380eb
Pitiedwzr <2629339380@qq.com>
2024-05-21 12:18:47 +1200

    Add save config fuction

--- settings.py
+++ settings.py
@@ -3,11 +3,27 @@ import yaml
 class commonClass:
     def __init__(self, raw):
         self.settings = raw
-        self.language = raw['language']
+
+    def __getattr__(self, item):
+        return self.settings.get(item)
+
+    def __setattr__(self, key, value):
+        if key == 'settings':
+            super().__setattr__(key, value)
+        else:
+            self.settings[key] = value
 
 class config:
     def __init__(self, raw):
         self.common = commonClass(raw['Common'])
 
+    def save(self):
+        data = {
+            'Common': self.common.settings
+        }
+        with open("settings.yaml", 'w') as f:
+            yaml.safe_dump(data, f)
+
+# Load the configuration
 with open("settings.yaml", "r") as f:
     config = config(yaml.safe_load(f))

5a3e4084e27e62e87b0d84495fbdc8c54e4f8b27
Pitiedwzr <2629339380@qq.com>
2024-05-28 11:40:50 +1200

    Cateforize files in the pending path

--- file_handling.py
+++ file_handling.py
@@ -1,11 +1,31 @@
 import os
+from collections import defaultdict
+
+class PendingFile:
+    def __init__(self, file_path):
+        self.path = file_path
+        self.name = os.path.basename(file_path)
+        self.ext = os.path.splitext(self.name)[1]
 
 def goThroughFiles(dir_path):
-    file_paths = []
-    file_names = []
-    for root, dirs, files in os.walk(dir_path):
-        for file in files:
+    files = []
+    for root, dirs, files_list in os.walk(dir_path):
+        for file in files_list:
             full_path = os.path.join(root, file)
-            file_paths.append(full_path)
-            file_names.append(file)
-    return file_paths, file_names
+            files.append(PendingFile(full_path))
+    return files
+
+def categorizeByExt(files):
+    categorized_files = defaultdict(list)
+    for file in files:
+        categorized_files[file.ext].append(file)
+    return categorized_files
+
+directory = "./for_assessment" # Will replace by the pending path
+files = goThroughFiles(directory)
+categorized_files = categorizeByExt(files)
+
+for ext, files in categorized_files.items():
+    print(f"Extension: {ext}")
+    for file in files:
+        print(f"  {file.path}")
